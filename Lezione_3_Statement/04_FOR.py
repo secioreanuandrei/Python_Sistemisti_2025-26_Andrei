24786e4f105c2a186244c83ce3971c40a5124c3b
secioreanuandrei <andrei.secioreanu@edu.immaginazioneelavoro.it>
2026-01-26 17:51:23 +0100

    quarto commit

--- Lezione_3_Statement/04_FOR.py
+++ Lezione_3_Statement/04_FOR.py
@@ -8,16 +8,30 @@ for i in range(0,46, 4):
     print(i)
 
 #Stampa tutti i numeri da 1 a 100. Quando stampi un multiplo di 3 stampa Zoom, quando stampa un multipli di 5 stampa Boom, quando stampi un multiplo di 3 e di 5 stampa Bangherang
+
+
 print("========  Giochino ZOOM BOOM Bangherang =========")
+
+quantZOOM = 0
+quantBOOM = 0
+quantBANGHERANG = 0
+
 for i in range(1,101):
     if i % 3 == 0 and i % 5 == 0: 
         print(i, "bangherang")
+        quantBANGHERANG += 1
     elif i % 3 == 0:
         print(i, "zoom")
+        quantZOOM += 1 
     elif i % 5 == 0:
         print(i, "BOOM")
+        quantBOOM += 1
     else:
         print(i)
 
-                      
+               
+print ( f"Il numero totale di ZOOM è {quantZOOM} multipli di 3 "  )
+print ( f"Il numero totale di BOOM è {quantBOOM} multipli di 5 "  ) 
+print ( f"Il numero totale di BANGHERANG è {quantBANGHERANG} multipli di 15 "  )
+
 
